3fc7f9c18acf852be1bb254324b9a6a7e27f444c
Lyzhin Iliya <lyzhin.iliya@gmail.com>
2020-08-10 17:22:41 +0300

    test 3

---
+++ 23.py
@@ -0,0 +1,9 @@
+import pyperclip
+from ctypes import windll
+
+pyperclip.copy('The text to be copied to the clipboard.')
+n = input() 
+if windll.user32.OpenClipboard(None):
+    windll.user32.EmptyClipboard()
+    windll.user32.CloseClipboard()
+n = input()
